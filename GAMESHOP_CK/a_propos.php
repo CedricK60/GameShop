d44d16bca9384ee98fe9ec81f40f67130bc5c82e
CedricK60 <117182473+CedricK60@users.noreply.github.com>
2023-06-06 21:56:37 +0200

    Update a_propos.php

--- GAMESHOP_CK/a_propos.php
+++ GAMESHOP_CK/a_propos.php
@@ -1,7 +1,7 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/GAMESHOP_CK/layouts/header.php'; ?>
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/GAMESHOP_CK/inc/db-function.php'; ?>
 
-<img src="assets/img/HibaPied.png" alt="Hiba aime les pieds">
+
 
 
 
